1b11f4c9e4ed8051e7c0442214f0d400592e20fd
roberto <r.magallanes@outlook.com>
2025-02-05 19:58:59 -0500

    changes

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -37,6 +37,8 @@ import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import { RequirePermission } from '@backstage/plugin-permission-react';
 import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
 
+import { githubAuthApiRef } from '@backstage/core-plugin-api';
+
 const app = createApp({
   apis,
   bindRoutes({ bind }) {
@@ -56,8 +58,22 @@ const app = createApp({
       catalogIndex: catalogPlugin.routes.catalogIndex,
     });
   },
+  // components: {
+  //   SignInPage: props => <SignInPage {...props} auto providers={['guest']} />,
+  // },
   components: {
-    SignInPage: props => <SignInPage {...props} auto providers={['guest']} />,
+    SignInPage: props => (
+      <SignInPage
+        {...props}
+        auto
+        provider={{
+          id: 'github-auth-provider',
+          title: 'GitHub',
+          message: 'Sign in using GitHub',
+          apiRef: githubAuthApiRef,
+        }}
+      />
+    ),
   },
 });
 
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -52,4 +52,6 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
 
+backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
+
 backend.start();
